1794d9a65dd01ff8b698c6425ee3ace4ccf1edbe
Ole Halvor Smylingsås <ohsmyl@gmail.com>
2025-04-11 20:27:56 +0000

    Blink 1 LED

---
+++ LedBlink/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Device.Gpio;
+
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+
+const int gpioPinNumber = 4;
+bool isLedOn = true;
+
+using var gpioController = new GpioController();
+gpioController.OpenPin(gpioPinNumber, PinMode.Output);
+
+while (true)
+{
+    gpioController.Write(gpioPinNumber, isLedOn ? PinValue.High : PinValue.Low);
+    Thread.Sleep(1500);
+    isLedOn = !isLedOn;
+}
+
+//gpioController.ClosePin(gpioPinNumber);
+
